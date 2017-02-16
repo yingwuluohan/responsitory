7076615eb07050101ef5c778de723d4a8dd56c2a
unknown <dongfangnan@Koolearn-inc.com>
2017-02-16 10:12:00 +0800

    new add

--- src/main/java/com/fang/service/impl/DemoServiceImpl.java
+++ src/main/java/com/fang/service/impl/DemoServiceImpl.java
@@ -5,6 +5,7 @@ import com.fang.service.DemoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
 import java.util.List;
 
 /**
@@ -13,7 +14,7 @@ import java.util.List;
 @Service
 public class DemoServiceImpl implements DemoService {
 
-    @Autowired
+    @Resource
     private DemoDao demoDao;
 
     public DemoDao getDemoDao() {
