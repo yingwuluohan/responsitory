768197d8a7434ac82db0a519ff6babd93ace0340
yingwuluohan <dfnwin@126.com>
2017-08-21 16:20:03 +0800

    add

--- web/src/main/java/com/java/spring/Test.java
+++ web/src/main/java/com/java/spring/Test.java
@@ -1,21 +1,10 @@
 package com.java.spring;
 
-import com.sun.tools.attach.AgentInitializationException;
-import com.sun.tools.attach.AgentLoadException;
-import com.sun.tools.attach.AttachNotSupportedException;
-import com.sun.tools.attach.VirtualMachine;
 
-import java.io.IOException;
 
 /**
  * Created by yingwuluohan on 2017/8/21.
  */
 public class Test {
 
-    public static void main(String[] args) throws AttachNotSupportedException,
-            IOException, AgentLoadException, AgentInitializationException {
-        VirtualMachine vm = VirtualMachine.attach(args[0]);
-        vm.loadAgent("/application/loadagent.jar");
-
-    }
 }
